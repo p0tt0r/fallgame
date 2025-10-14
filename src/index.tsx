0cc25ee049e404cedd606d65e3cc1c7b90ebfa38
p0tt0r <e89889936485@gmail.com>
2025-10-15 02:38:32 +0300

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/game.css';
+import './styles/game.desktop.css';
+import './styles/game.mobile.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
